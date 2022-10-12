945a02e250f003bd338d3511894d54fe4c238fc1
Marco Antonio Gomez Gutierrez <anthonyansoni24@gmail.com>
2022-10-11 23:01:10 -0400

    Primer Commit

---
+++ src/main/java/com/emergente/Producto.java
@@ -0,0 +1,59 @@
+
+package com.emergente;
+
+
+public class Producto {
+    private int id;
+    private String producto;
+    private int cantidad;
+    private double precio;
+
+    public Producto() {
+        id=0;
+        producto="";
+        cantidad=0;
+        precio=0;
+               
+               
+    }
+
+    public Producto(int id, String producto, int cantidad, double precio) {
+        this.id = id;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+}
